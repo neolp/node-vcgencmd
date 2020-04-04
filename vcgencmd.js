f83ff507d2c6de52840acb5b51f6267039ba65d0
Alexei Tsigulin <mail@neolp.com>
2020-04-04 17:36:54 +0700

    add throttled function

--- vcgencmd.js
+++ vcgencmd.js
@@ -4,13 +4,35 @@ var binding = require('./build/Release/binding');
 var request = binding.request;
 
 
+exports.request = request;
+
+/**
+ 
+ */
+exports.throttled = function () {
+    var answer = request('get_throttled');
+    var bits = parseInt(answer.slice(answer.indexOf('=') + 1));
+    var ret = new Set()
+
+    if (bits & 1) ret.add('undervoltage') //bit 0 Under - voltage detected
+    if (bits & 2) ret.add('freqcapped') //bit 1	Arm frequency capped
+    if (bits & 4) ret.add('throttled') //bit 2	Currently throttled
+    if (bits & 8) ret.add('templimit') //bit 3	Soft temperature limit active
+    if (bits & 65536) ret.add('undervoltagelatch') //bit 16	Under - voltage has occurred
+    if (bits & 131072) ret.add('freqcappedlatch') //bit 17	Arm frequency capping has occurred
+    if (bits & 262144) ret.add('throttledlatch') //bit 18	Throttling has occurred
+    if (bits & 524288) ret.add('templimitlatch') //bit 19	Soft temperature limit has occurred
+
+    return ret;
+};
+
 /**
  * Measure clock frequency.
  * @param  {string} clock one of 'arm', 'core', 'h264', 'isp', 'v3d', 'uart',
  *                        'pwm', 'emmc', 'pixel', 'vec', 'hdmi', 'dpi'
  * @return {number}
  */
-exports.measureClock = function(clock) {
+exports.measureClock = function (clock) {
     var answer = request('measure_clock ' + clock);
 
     // 'frequency(0)=0' or 'error=2 error_msg="Invalid arguments"'
@@ -25,7 +47,7 @@ exports.measureClock = function(clock) {
  * @param  {string} [id='core'] one of 'core', 'sdram_c', 'sdram_i', 'sdram_p'
  * @return {number}
  */
-exports.measureVolts = function(id) {
+exports.measureVolts = function (id) {
     var answer = request('measure_volts ' + (id || ''));
 
     // 'bad arguments'
@@ -39,7 +61,7 @@ exports.measureVolts = function(id) {
  * Measure core temperature of BCM2835 SoC.
  * @return {number}
  */
-exports.measureTemp = function() {
+exports.measureTemp = function () {
     return parseFloat(request('measure_temp').slice(5));
 };
 
@@ -48,7 +70,7 @@ exports.measureTemp = function() {
  * @param  {string} codec one of 'H264', 'MPG2', 'WVC1', 'MPG4', 'MJPG', 'WMV9'
  * @return {boolean}
  */
-exports.codecEnabled = function(codec) {
+exports.codecEnabled = function (codec) {
     switch (codec) {
         case 'H264':
         case 'MPG2':
@@ -70,16 +92,16 @@ exports.codecEnabled = function(codec) {
  * @param  {string} config specific option, 'int' or 'str'
  * @return {Object|number|string}
  */
-exports.getConfig = function(config) {
+exports.getConfig = function (config) {
     if (!config)
         throw new Error('config is incorrect');
-    
+
     var isNumMap = config === 'int',
         isMap = isNumMap || config === 'str',
         answer = request('get_config ' + config).trim();
 
     if (isMap)
-        return answer.split('\n').reduce(function(res, line) {
+        return answer.split('\n').reduce(function (res, line) {
             var pair = line.split('=');
             res[pair[0]] = isNumMap ? +pair[1] : pair[1];
             return res;
@@ -97,11 +119,11 @@ exports.getConfig = function(config) {
  * Get info about availability of camera.
  * @return {Object} {supported, detected}
  */
-exports.getCamera = function() {
+exports.getCamera = function () {
     var answer = request('get_camera');
     return {
         supported: answer[10] === '1',
-        detected:  answer[21] === '1'
+        detected: answer[21] === '1'
     };
 };
 
@@ -110,7 +132,7 @@ exports.getCamera = function() {
  * @param  {string} mem 'arm' or 'gpu'
  * @return {number}
  */
-exports.getMem = function(mem) {
+exports.getMem = function (mem) {
     if (!(mem === 'arm' || mem === 'gpu'))
         throw new Error('mem is incorrect');
 
@@ -121,22 +143,22 @@ exports.getMem = function(mem) {
  * Get height, width, and depth of the display framebuffer
  * @return {Object} {width, height, depth}
  */
-exports.getLCDInfo = function() {
+exports.getLCDInfo = function () {
     var info = request('get_lcd_info').split(' ');
     return {
-        width:  +info[0],
+        width: +info[0],
         height: +info[1],
-        depth:  +info[2]
+        depth: +info[2]
     };
 };
 
 /**
  * Flush GPU's L1 cache
  */
-exports.cacheFlush = function() {
+exports.cacheFlush = function () {
     request('cache_flush');
 };
 
-process.on('exit', function() {
+process.on('exit', function () {
     binding.disconnect();
 });
